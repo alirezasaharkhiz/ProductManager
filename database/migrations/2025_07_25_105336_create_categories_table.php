19d5c51fdbb5ac434fd603dfad8d450694c6e6d9
alirezasaharkhiz <alireza.saharkhiz@outlook.com>
2025-07-25 16:58:29 +0330

    Update migrations

--- database/migrations/2025_07_25_105336_create_categories_table.php
+++ database/migrations/2025_07_25_105336_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Kalnoy\Nestedset\NestedSet;
 
 return new class extends Migration
 {
@@ -11,7 +12,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
+            NestedSet::columns($table);
+//            $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
